3e4e87acae8de0f7577f78696d6fd640677225e1
Emanuele Rovaris <emanuelerovaris@fattureincloud.it>
2023-03-08 16:28:49 +0100

    feat: process env support

--- src/services/posts.service.ts
+++ src/services/posts.service.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 import {IPost} from "../models/posts.model";
 
 export async function getPosts(): Promise<IPost[]> {
-    const response = await axios.get('http://localhost:8081/posts');
+    const response = await axios.get(`${import.meta.env.VITE_API_PATH}/posts`);
     return response.data;
 }
