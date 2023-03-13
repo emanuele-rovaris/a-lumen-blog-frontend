e7cd65883797a2219298fc33491dfa67990ebdee
Emanuele Rovaris <emanuelerovaris@fattureincloud.it>
2023-03-13 09:20:07 +0100

    fix: retrieve posts loading

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,12 +6,16 @@ import {Card} from "@/components";
 import {setNotificationAction} from "@/store/slices/notificationSlice";
 import {NOTIFICATION_STATUS} from "@/utils";
 import {useAppDispatch} from "@/store/store";
+import {useOutletContext} from "react-router-dom";
+import {LoadingContext} from "@/App";
 
 const Home: React.FC = () => {
     const [posts, setPosts] = useState<IPost[]>([]);
     const dispatch = useAppDispatch();
+    const {setLoading} = useOutletContext<LoadingContext>();
 
     useEffect(() => {
+        setLoading(true);
         getPosts()
             .then(res => setPosts(res))
             .catch(err => {
@@ -20,7 +24,8 @@ const Home: React.FC = () => {
                     open: true,
                     message: `Error retrieving posts!\n${err.message}`,
                 }));
-            });
+            })
+            .finally(() => setLoading(false));
     }, []);
 
     return (
