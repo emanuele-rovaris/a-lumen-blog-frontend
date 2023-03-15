959153f1817cc7cee9bd198d883bebc11bcc5869
Emanuele Rovaris <emanuelerovaris@fattureincloud.it>
2023-03-15 09:54:27 +0100

    test: home page update

--- src/tests/pages/Home.test.tsx
+++ src/tests/pages/Home.test.tsx
@@ -1,16 +1,25 @@
-import {describe, Mock, vi} from "vitest";
+import {afterEach, describe, Mock, vi} from "vitest";
 import {postsStub} from "@/tests/stubs/post.stub";
 import axios from "axios";
-import {render, screen, waitFor} from "@testing-library/react";
+import {fireEvent, render, screen, waitFor} from "@testing-library/react";
 import {Home} from "@/pages";
 import React from "react";
 import {RenderRouteWithOutletContext} from "../utils/outlet-context.utils";
 import {AnyAction} from "redux";
 import {useDispatch} from "react-redux";
+import {useNavigate} from "react-router-dom";
 
 describe('Home page', () => {
     const fakeData = postsStub();
     const mockOutletContextData = {setLoading: () => true};
+
+    // vi.mock("react-router-dom", async () => {
+    //     const actual: any = await vi.importActual("react-router-dom")
+    //     return {
+    //         ...actual,
+    //         useNavigate: vi.fn(),
+    //     }
+    // });
 
     vi.mock("react-redux", async () => {
         const actual: any = await vi.importActual("react-redux");
@@ -19,16 +28,20 @@ describe('Home page', () => {
             useDispatch: vi.fn(),
         }
     });
-    const dispatchResultRecorder = {} as any;
+
+    const dispatchResultRecorder: { [key: string]: any } = {};
     const fakeDispatch = (action: AnyAction) => {
         dispatchResultRecorder[action.type] = action.payload;
     };
     const useDispatchMock = useDispatch as Mock;
     useDispatchMock.mockImplementation(() => fakeDispatch);
 
+    afterEach(() => {
+        useDispatchMock.mockClear();
+    });
 
     it('should retrieve and render posts list', async () => {
-        vi.spyOn(axios, "get").mockImplementation(() =>
+        vi.spyOn(axios, "get").mockImplementationOnce(() =>
             Promise.resolve({data: fakeData}),
         );
         render(
@@ -36,13 +49,12 @@ describe('Home page', () => {
                 <Home/>
             </RenderRouteWithOutletContext>
         );
-        const firstPost = await waitFor(() => screen.getByText(fakeData[0].title))
+        const firstPost = await waitFor(() => screen.getByText(fakeData[0].title));
         expect(firstPost).toBeInTheDocument();
     });
 
     it('should not render posts if api fails and dispatch redux action', async () => {
-        useDispatchMock.mockClear();
-        vi.spyOn(axios, "get").mockImplementation(() =>
+        vi.spyOn(axios, "get").mockImplementationOnce(() =>
             Promise.reject({message: 'Error'}),
         );
         render(
@@ -55,5 +67,19 @@ describe('Home page', () => {
         expect(useDispatchMock).toHaveBeenCalledTimes(1);
         expect(dispatchResultRecorder['notification/setNotificationAction'])
             .toEqual({status: 'error', open: true, message: 'Error retrieving posts!\nError'});
+    });
+
+    it('should navigate to post detail on click', async () => {
+        // vi.spyOn(axios, "get").mockImplementationOnce(() =>
+        //     Promise.resolve({data: fakeData}),
+        // );
+        // render(
+        //     <RenderRouteWithOutletContext context={mockOutletContextData}>
+        //         <Home/>
+        //     </RenderRouteWithOutletContext>
+        // );
+        // const firstPost = await waitFor(() => screen.getByText(fakeData[0].title));
+        // fireEvent.click(firstPost);
+        // expect(useNavigate).toHaveBeenCalledWith('');
     });
 });
